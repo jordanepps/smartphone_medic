378ba84c0d5f45b6914659ae56716174fb84befe
Jordan Epps <jordanepps@gmail.com>
2019-10-29 15:57:36 -0400

    Add logo to header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import Logo from '../../imgs/logo.png';
+import './Header.css';
 
 export default function Header() {
   return (
     <header>
-      <Link to="/">
+      <Link to="/" className="main-logo">
         <img src={Logo} alt="Smartphone Medic Logo" />
       </Link>
       <nav>
