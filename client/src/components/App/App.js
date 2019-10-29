7fd04e2f70e27d9595c7746ba3f191ddd1b94109
Jordan Epps <jordanepps@gmail.com>
2019-10-29 14:47:45 -0400

    Create route files for home and dashboard

---
+++ client/src/components/App/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router';
+import './App.css';
+
+export default function App() {
+  return <div>TEST</div>;
+}
---
+++ client/src/routes/Dashboard/Dashboard.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Dashboard.css';
+
+export default function Dashboard() {
+  <div>Dashboard</div>;
+}
