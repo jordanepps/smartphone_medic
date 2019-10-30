ecc15aa4fe90c380be533669a5113108873a24a5
Jordan Epps <jordanepps@gmail.com>
2019-10-30 13:03:35 -0400

    Make footer component

--- client/src/layouts/MainLayout/MainLayout.js
+++ client/src/layouts/MainLayout/MainLayout.js
@@ -3,6 +3,7 @@ import Header from '../../components/Header/Header';
 import SideDrawer from '../../components/SideDrawer/SideDrawer';
 import './MainLayout.css';
 import Backdrop from '../../components/Backdrop/Backdrop';
+import Footer from '../../components/Footer/Footer';
 
 export default function MainLayout({ children }) {
   const [sideDrawerOpen, setSideDrawer] = useState(false);
@@ -17,7 +18,7 @@ export default function MainLayout({ children }) {
       <SideDrawer show={sideDrawerOpen} close={backdropClickHandler} />
       {sideDrawerOpen && <Backdrop click={backdropClickHandler} />}
       <main className="main-layout-container">{children}</main>
-      <footer>Footer</footer>
+      <Footer />
     </>
   );
 }
