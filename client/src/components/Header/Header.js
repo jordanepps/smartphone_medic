42b1434ae3bbd379db476a5c5d40fe838106e7d0
Jordan Epps <jordanepps@gmail.com>
2019-11-02 13:08:29 -0400

    Fix header logo on mobile

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,16 +8,14 @@ import './Header.css';
 export default function Header({ drawerHandler }) {
   const [atTop, setAtTop] = useState(true);
 
-  window.addEventListener('scroll', function() {
-    setAtTop(window.pageYOffset > 0 ? false : true);
-  });
+  window.addEventListener('scroll', () =>
+    setAtTop(window.pageYOffset > 0 ? false : true)
+  );
 
   return (
     <header className={atTop ? 'main-header' : 'main-header scrolled'}>
       <div className="main-header-container">
-        <div>
-          <ToggleBtn click={drawerHandler} />
-        </div>
+        <ToggleBtn click={drawerHandler} />
         <Link to="/" className="main-logo">
           <img src={Logo} alt="Smartphone Medic Logo" />
         </Link>
