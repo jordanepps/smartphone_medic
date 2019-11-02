b42b2d7de95a6211321f5313c4c9bb524c6948f7
Jordan Epps <jordanepps@gmail.com>
2019-11-02 15:14:33 -0400

    Create template for server

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+
+app.use((error, req, res, next) =>
+  res.status(500).json({ message: error.message, error })
+);
+
+module.exports = app;
