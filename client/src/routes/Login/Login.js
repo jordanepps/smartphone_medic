5dae8bdffe38713348e64b108c2420a4aa8e2180
Jordan Epps <jordanepps@gmail.com>
2019-11-04 16:41:47 -0500

    Add function to tab switch

--- client/src/routes/Login/Login.js
+++ client/src/routes/Login/Login.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Email, Password, SubmitButton } from '../../components/Utils/Utils';
+import './SignupLogin.css';
 
 export default function Login() {
+  const [tabIsLogin, setTabIsLogin] = useState(true);
+
+  const handleLoginSelect = () => setTabIsLogin(true);
+  const handleSignUpSelect = () => setTabIsLogin(false);
+
   return (
     <div className="dashboard-form-container">
       <form className="dashboard-form">
-        <h2>Login</h2>
-        <Email />
-        <Password />
-        <SubmitButton value="log in" />
-        <div className="form-footer">
-          <Link>Forgot Password?</Link>
+        <div className="tab-btn-container">
+          <h2
+            tabIndex={1}
+            class={`tab tab-signup ${!tabIsLogin && 'current-tab'}`}
+            onClick={handleSignUpSelect}
+          >
+            Sign Up
+          </h2>
+          <h2
+            tabIndex={1}
+            class={`tab tab-login ${tabIsLogin && 'current-tab'}`}
+            onClick={handleLoginSelect}
+          >
+            Login
+          </h2>
+        </div>
+        <div className="display">
+          <Email />
+          <Password />
+          <SubmitButton value="log in" />
+          <div className="form-footer">
+            {/* TODO:Place outline around forget Password */}
+            <Link>Forgot Password?</Link>
+          </div>
         </div>
       </form>
     </div>
