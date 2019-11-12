d0435761b5c10bb7ccfb6852ae6d79d268652bae
Jordan Epps <jordanepps@gmail.com>
2019-11-12 14:49:54 -0500

    Begin to add logic for login component

--- client/src/components/TabDisplay/Login.js
+++ client/src/components/TabDisplay/Login.js
@@ -3,8 +3,14 @@ import { Link } from 'react-router-dom';
 import { Email, Password, SubmitButton } from '../Utils/Utils';
 
 export default function Login() {
+  const handleLogin = e => {
+    e.preventDefault();
+    const { email, password } = e.target;
+    console.log(email.value, password.value);
+  };
+
   return (
-    <form className="display">
+    <form className="display" onSubmit={handleLogin}>
       <Email />
       <Password />
       <SubmitButton value="log in" />
