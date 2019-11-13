04e1469a5074246cacb3c2ee1ce30849faa8cdf7
Jordan Epps <jordanepps@gmail.com>
2019-11-13 10:42:29 -0500

    Add sudo code for auth

--- server/routes/auth/auth.router.js
+++ server/routes/auth/auth.router.js
@@ -4,6 +4,18 @@ const authRouter = express.Router();
 
 authRouter.post('/', (req, res, next) => {
   const { email, password } = req.body;
+  const loginData = { email, password };
+
+  //Check for required fields in request body
+  for (const [key, value] of Object.entries(loginData))
+    if (value == null)
+      return res
+        .status(400)
+        .json({ error: `Missing '${key}' in request body` });
+
+  //Check if account is deleted
+  //Check password
+  //verify user
 });
 
 module.exports = authRouter;
