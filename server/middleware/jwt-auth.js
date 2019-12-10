f839f0e1d4c729cc89bdf26df8c98fa60eb1862f
Jordan Epps <jordanepps@gmail.com>
2019-12-10 11:15:30 -0500

    Create jwt middleware

---
+++ server/middleware/jwt-auth.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+function requireAuth(req, res, next) {
+  const token = req.header('x-auth-token');
+  //Check for token
+  if (!token) res.status(401).json({ error: 'Unauthorized Request' });
+
+  try {
+    //Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    //Add user to payload
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(400).json({ error: 'Token is not valid' });
+  }
+}
+
+module.exports = { requireAuth };
