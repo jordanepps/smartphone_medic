f874ebbbc2d516978e9123c2e070349c66fc4433
Jordan Epps <jordanepps@gmail.com>
2020-01-02 11:15:59 -0500

    Add test data to home cards

--- client/src/components/HomeCardContainer/HomeCardContainer.js
+++ client/src/components/HomeCardContainer/HomeCardContainer.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './HomeCardContainer.css';
-// import HomeCard from '../HomeCard/HomeCard';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 function HomeCard() {
-  return <div className="home-card"></div>;
+  return (
+    <div className="home-card">
+      <FontAwesomeIcon icon={faCoffee} />
+      <h4 className="card-title">Test Message</h4>
+      <p className="card-body">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dolorum
+        illum qui consequatur distinctio provident.
+      </p>
+    </div>
+  );
 }
 
 export default function HomeCardContainer() {
