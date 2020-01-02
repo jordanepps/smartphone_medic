1e2ed8c022e8aad47b172a7a6a424d6ad96a4b49
Jordan Epps <jordanepps@gmail.com>
2020-01-02 09:51:57 -0500

    Reduce home card and home container components to one file

--- client/src/components/HomeCard/HomeCard.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './HomeCard.css';
-
-export default function HomeCard() {
-  return <div className="home-card"></div>;
-}
--- client/src/components/HomeCardContainer/HomeCardContainer.js
+++ client/src/components/HomeCardContainer/HomeCardContainer.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './HomeCardContainer.css';
-import HomeCard from '../HomeCard/HomeCard';
+// import HomeCard from '../HomeCard/HomeCard';
+
+function HomeCard() {
+  return <div className="home-card"></div>;
+}
 
 export default function HomeCardContainer() {
   return (
